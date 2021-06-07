2402cafb56b5a3cdf99c53ed2b012c458bd3193e
rr2x <79620740+rr2x@users.noreply.github.com>
2021-06-07 18:21:40 +0800

    comments updated

--- articles/admin.py
+++ articles/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comment
 
-admin.site.register(Article)
+
+class CommentInLine(admin.TabularInline):  # or can use admin.StackedInLine
+    model = Comment
+    extra = 0  # so that no extra empty forms below the comment list
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInLine,
+    ]
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
--- articles/models.py
+++ articles/models.py
@@ -19,3 +19,22 @@ class Article(models.Model):
 
     def get_absolute_url(self):
         return reverse('article_detail', args=[str(self.id)])
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(
+        Article,
+        on_delete=CASCADE,
+        related_name='comments'
+    )
+    comment = models.CharField(max_length=140)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=CASCADE,
+    )
+
+    def __str__(self):
+        return self.comment
+
+    def get_absolute_url(self):
+        return reverse('article_list')
